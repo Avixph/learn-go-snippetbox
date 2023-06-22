500eaed93c47f47532afb0b0ce5cfd550fd6f69f
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-06-22 18:33:44 -0400

    Centralized error handling and routing so that we avoid repeating ourself when writing code

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,7 +6,9 @@ import (
 	"runtime/debug"
 )
 
-// The serverError helper writes an error message and stack trace to the errorLog, then sends a generic 500 Internal Server Error response to the user.
+// The serverError helper writes an error message and stack trace to the
+// errorLog, then sends a generic 500 Internal Server Error response to the
+// user.
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
@@ -15,12 +17,15 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	http.Error(w, http.StatusText(ise), ise)
 }
 
-// The clientError helper sends a specific status code and corresponding description to the user. We'll use this later to send responses like 400 "Bad Request" when there's a problem with the request the user sent.
+// The clientError helper sends a specific status code and corresponding
+// description to the user. We'll use this later to send responses like 400
+// "Bad Request" when there's a problem with the request the user sent.
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
-// We'll also implement a notFound helper. This is simply a convenience wrapper around clientError which sends a 404 Not Found response to the user.
+// We'll also implement a notFound helper. This is simply a convenience 
+// wrapper around clientError which sends a 404 Not Found response to the user.
 func (app *application) notFound(w http.ResponseWriter) {
 	nf := http.StatusNotFound
 	app.clientError(w, nf)
