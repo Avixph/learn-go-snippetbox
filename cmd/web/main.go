22277a7a5a3c1d687854621a5b0ba9c7386f7cd3
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-06-22 16:45:12 -0400

    improved app log messages to include more information, and manage them differently depending on the type (or level) of log message

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -19,18 +20,29 @@ func main() {
 	// errs are encounted during parsing the application will be terminated.
 	flag.Parse()
 
+	// Use log.New() to create a logger for writting info messages. This
+	// takes three parameters: the destination to wrie the logs to (os.
+	// Stdout), a string prefix for message (INFO followed by tab), and
+	// flags to indicate what additional info ito include (local data and
+	// time). Note thatthe flags ae joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writting error messages in the same way, but use
+	// stderr as the destination and use the log.Lshortfile to include the
+	// relevant file name and line number.
+	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// Use the http.NewServerMux() func to initialize a new servermux.
 	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static"
 	// directory. Note that the path given to the http.Dir func is relative
 	// to the project directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	// Use the mux.handle() func to register the file server as the handler
 	// for all URL paths that start with "/static/". For matching paths, we
 	// strip the "/static/" prefix before the request reaches the file
 	// server.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Register the home, snippetView and snippetCreate funcs as handlers
@@ -39,18 +51,28 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	// Initalize a new http.Server struct. We set the Addr and Handler
+	// fields so that the server uses the same network address and routes as
+	// before, and set the ErrorLog fielfd so that ther server now uses the
+	// custom errorLog logger in the event of and problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// The value returned from the flag.Sring() func is a pointer to the
 	// flag value, not the value itself. So we need to dereference the
 	// pointer (i.e. prefix it with the * symbol) before using it. Note that
-	// we're using the log.Printf() func to interpolate the address with the
-	// log message.
-	// Use the http.ListenAndServe() func to start a new web server. We pass
-	// in two parameters: the TCP network address to listen on (ex:
-	// (localhost::4000)) and the servermux we created. If http.
-	// listenAndServe() returns an err we use the log.Fatal() func to log
-	// the err message and exit. Note that any err returned by http.
-	// listenAndServe() is always non-nill.
-	log.Printf("Starting server on http://localhost%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// we're using the infoLog.Printf() func to interpolate the address with
+	// the log message.
+	// Use the http.ListenAndServe() func on the http.Server() struct to
+	// start a new web server. We pass in two parameters: the TCP network
+	// address to listen on (ex:(localhost::4000)) and the servermux we
+	// created. If http. listenAndServe() returns an err we use the errorLog.
+	// Fatal() func to log the err message and exit. Note that any err
+	// returned by http. listenAndServe() is always non-nill.
+	infoLog.Printf("Starting server on http://localhost%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
