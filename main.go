5d9f33b89c63685dfe2edc70f0cbea22c80606c8
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-06-21 23:07:51 -0400

    added two additional route requests

--- main.go
+++ main.go
@@ -8,14 +8,38 @@ import (
 // Define a home handler func that writes a byte slice containing
 // "Hello from Snippetbox!" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If not, use
+	// the htp.NotFound() func to send a 404 respond to the client.
+	// Importantly, we then return from the handler. If we don't return, the
+	// handler would keep executing and also write "Hello from Snippetbox!" message
+	rup := r.URL.Path
+	if rup != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox!"))
 }
 
+// Define a snippetView handler func
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Displaying a specific snippet..."))
+}
+
+// Define snippetCreate handler func
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
-	// Use tghe http.NewServerMux() func to initialize a new servermux, then
+	// Use the http.NewServerMux() func to initialize a new servermux, then
 	// register the home func as a handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	// Register the snippetView and snippetCreate handler funcs and
+	// corrisponding URL patrerns with the servermux.
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() func to start a new web server. We pass
 	// in two parameters: the TCP network address to listen on (ex:(localhost::4000))
