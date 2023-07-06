094ef2ec29a62dc336a7c0783f20610ffc55c6cd
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-07-06 03:45:32 -0400

    Passed dynamic data to the HTML templates and used various actions/functions in Go's html/tmplate package to contrrol the dyisplay of that data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,8 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 
-	// "html/template"
 	"net/http"
 
 	"github.com/Avixph/learn-go-snippetbox/internal/models"
@@ -33,43 +33,50 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
+	// for _, snippet := range snippets {
+	// 	fmt.Fprintf(w, "%+v\n", snippet)
+	// }
 
 	// Initialize a slice containing the paths to the two templates. It's
 	// important to note that the file containing our base template must be
 	// the "first" file in the slice.
-	// templateFiles := []string{
-	// 	"./ui/html/base.html",
-	// 	"./ui/html/components/nav.html",
-	// 	"./ui/html/pages/home.html",
-	// 	"./ui/html/components/footer.html",
-	// }
+	tmplFiles := []string{
+		"./ui/html/base.html",
+		"./ui/html/components/nav.html",
+		"./ui/html/pages/home.html",
+		"./ui/html/components/footer.html",
+	}
 
 	// Use the template.ParseFiles() func to read the template files and
 	// store the templates in a template set. If there's an err, we log a
 	// detailed err message and use the http.Error() func to send a generic
 	// 500 Interanl Server Err response to the user.
-	// ts, err := template.ParseFiles(templateFiles...)
-	// if err != nil {
-	// 	// Because the home handler func is now a method against application
-	// 	// it can access it's feilds, including the error loger. We'll write
-	// 	// the log message to this instead of the standard logger.
-	// 	// Use ther serverError() helper
-	// 	app.serverError(w, err)
-	// 	return
-	// }
+	ts, err := template.ParseFiles(tmplFiles...)
+	if err != nil {
+		// Because the home handler func is now a method against application
+		// it can access it's feilds, including the error loger. We'll write
+		// the log message to this instead of the standard logger.
+		// Use ther serverError() helper
+		app.serverError(w, err)
+		return
+	}
+
+	// Create an instance of a templateData struct holding the slice of
+	// snippets.
+	tmplData := &templateData{
+		Snippets: snippets,
+	}
 
 	// We then use the ExecuteTemplate() method to write the content of the
 	// "base" template as the response body.
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	// Also update the code hre to use the error logger from the
-	// 	// application struct.
-	// 	// Use the app.serverError() helper
-	// 	app.serverError(w, err)
-	// }
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", tmplData)
+	if err != nil {
+		// Also update the code hre to use the error logger from the
+		// application struct.
+		// Use the app.serverError() helper
+		app.serverError(w, err)
+	}
 }
 
 // Define a snippetView handler func
@@ -98,9 +105,35 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the fmt.Fprintf() func to write the snippet data as a plain-text
-	// HTTP response body.
-	fmt.Fprintf(w, "%+v", snippet)
+	// Initialize a slice containing the paths to the view.html file, plus the
+	// base layout and navigation partial we previously made.
+	tmplFiles := []string{
+		"./ui/html/base.html",
+		"./ui/html/components/nav.html",
+		"./ui/html/pages/view.html",
+		"./ui/html/components/footer.html",
+	}
+
+	// Parse the template files...
+	ts, err := template.ParseFiles(tmplFiles...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Create an instanse of a templateData struct holding the snippet data.
+	tmplData := &templateData{
+		Snippet: snippet,
+	}
+
+	// We then use the ExecuteTemplate() method to write the content of the
+	// "base" template as the response body. Notice how we are passing in the
+	// snippet data (a templateData struct) as the final parameter.
+	err = ts.ExecuteTemplate(w, "base", tmplData)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 }
 
 // Define snippetCreate handler func
