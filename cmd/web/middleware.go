ac8af6e0c3fc6136a5788a55f20e0d7298e80d4f
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-07-12 15:45:58 -0400

    Created a middleware that recovers panics so that they are gracefully handled by the app

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func secureHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +24,26 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred func that will always be run in the event of a panic as Go
+		// unwinds the stack.
+		defer func() {
+			// Use the builtin recover func to check if there has been a panic or not.
+			// If there is...
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header to the response.
+				w.Header().Set("Connection", "close")
+				// Call the app.serverError helper method to return a 500 Internal Server
+				// Response.
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+
 	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,6 +27,7 @@ func (app *application) routes() http.Handler {
 	// Pass ther servermux as the 'next' parameter to the secureheaders middleware.
 	// Because secureHeaders is just a func, and the func returns an http.Handler we
 	// don't need to do anything else.
-	// Wrap the existing chain with the logRequest middleware.
-	return app.logRequest(secureHeader(mux))
+	// Wrap the existing chain with the recoverPanic middleware then the logRequest middleware.
+	return app.recoverPanic(app.logRequest(secureHeader(mux)))
+
 }
